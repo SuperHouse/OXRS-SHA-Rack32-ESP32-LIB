a1645ec3da1031cd8789e7dd6aae9a73c3dec41f
Ben Jones <sumnerboy12@users.noreply.github.com>
2021-10-01 10:36:35 +1300

    Changes to work with updated MQTT lib (#3)
    * update to work with mqtt lib changes
    * standardise serial logging
    * revert to explicit setClientId() overloads

--- src/OXRS_Rack32.cpp
+++ src/OXRS_Rack32.cpp
@@ -24,15 +24,6 @@ OXRS_MQTT _mqtt(_mqttClient);
 // Temp sensor
 Adafruit_MCP9808 _tempSensor;
 
-OXRS_Rack32::OXRS_Rack32(const char * fwName, const char * fwShortName, const char * fwMakerCode, const char * fwVersion, const char * fwCode)
-{
-  _fwName       = fwName;
-  _fwShortName  = fwShortName;
-  _fwMakerCode  = fwMakerCode;
-  _fwVersion    = fwVersion;  
-  _fwCode       = fwCode;
-}
-
 void _mqttCallback(char * topic, byte * payload, int length) 
 {
   // Indicate we have received something on MQTT
@@ -42,9 +33,18 @@ void _mqttCallback(char * topic, byte * payload, int length)
   _mqtt.receive(topic, payload, length);
 }
 
+OXRS_Rack32::OXRS_Rack32(const char * fwName, const char * fwShortName, const char * fwMakerCode, const char * fwVersion, const char * fwCode)
+{
+  _fwName       = fwName;
+  _fwShortName  = fwShortName;
+  _fwMakerCode  = fwMakerCode;
+  _fwVersion    = fwVersion;  
+  _fwCode       = fwCode;
+}
+
 void OXRS_Rack32::setMqttBroker(const char * broker, uint16_t port)
 {
-  _mqttClient.setServer(broker, port);
+  _mqtt.setBroker(broker, port);
 }
 
 void OXRS_Rack32::setMqttAuth(const char * username, const char * password)
@@ -98,10 +98,13 @@ void OXRS_Rack32::begin(jsonCallback config, jsonCallback command)
   byte mac[6];
   _initialiseEthernet(mac);
   
-  // Set MQTT client id and callbacks
+  // Set the MQTT client id and register our callbacks
   _mqtt.setClientId(_fwCode, mac);
   _mqtt.onConfig(config);
   _mqtt.onCommand(command);
+  
+  // Start the MQTT handler
+  _mqtt.begin();
 
   // Display the MQTT topic
   char topic[64];
@@ -168,7 +171,7 @@ boolean OXRS_Rack32::publishTelemetry(JsonObject json)
 void OXRS_Rack32::_initialiseEthernet(byte * ethernetMac)
 {
   // Determine MAC address
-  Serial.print(F("Getting Ethernet MAC address from ESP32: "));
+  Serial.print(F("[eth ] getting MAC address from ESP32..."));
   WiFi.macAddress(ethernetMac);  // Temporarily populate Ethernet MAC with ESP32 Base MAC
   ethernetMac[5] += 3;           // Ethernet MAC is Base MAC + 3 (see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/system.html#mac-address)
 
@@ -181,7 +184,7 @@ void OXRS_Rack32::_initialiseEthernet(byte * ethernetMac)
   Ethernet.init(ETHERNET_CS_PIN);
 
   // Reset the Wiznet Ethernet chip
-  Serial.print("Resetting Wiznet W5500 Ethernet chip...");
+  Serial.print("[eth ] resetting Wiznet W5500...");
   pinMode(WIZNET_RESET_PIN, OUTPUT);
   digitalWrite(WIZNET_RESET_PIN, HIGH);
   delay(250);
@@ -192,7 +195,7 @@ void OXRS_Rack32::_initialiseEthernet(byte * ethernetMac)
   Serial.println("done");
 
   // Obtain IP address
-  Serial.print(F("Getting IP address via DHCP: "));
+  Serial.print(F("[eth ] getting IP address via DHCP..."));
   if (Ethernet.begin(ethernetMac, DHCP_TIMEOUT_MS, DHCP_RESPONSE_TIMEOUT_MS))
   {
     // Display IP address on serial
@@ -201,13 +204,13 @@ void OXRS_Rack32::_initialiseEthernet(byte * ethernetMac)
   else
   {
     // Unable to obtain a DHCP lease
-    Serial.println(F("FAILED!"));
+    Serial.println(F("failed"));
   }
 }
 
 void OXRS_Rack32::_initialiseTempSensor()
 {
-  Serial.println(F("Scanning for temperature sensor on I2C bus..."));
+  Serial.println(F("[i2c ] scanning for temperature sensor..."));
   Serial.print(F(" - 0x"));
   Serial.print(MCP9808_I2C_ADDRESS, HEX);
   Serial.print(F("..."));
